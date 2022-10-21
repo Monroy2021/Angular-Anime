782fbee6b76871acb1b793dae8b9f2336c15abdd
Monroy2021 <luismonroy1014@gmail.com>
2022-10-21 12:15:59 -0500

    Creacion del proyecto mas la creacion de los componente y estilos css

---
+++ src/app/components/search-anime/search-anime.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-anime',
+  templateUrl: './search-anime.component.html',
+  styleUrls: ['./search-anime.component.css']
+})
+export class SearchAnimeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  search(searchTerm: string) {
+    console.log(searchTerm)
+  }
+
+}
